2708f7b93fdc3cb6315844a55da9a2628f0cc032
Ermesom Matheus <45245621+Ermesom007@users.noreply.github.com>
2019-11-05 01:05:46 -0300

    Primeiro Commit
    Estrutura do Projeto Criado

---
+++ Aula12_JDBC/build/web/index.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : index
+    Created on : 04/11/2019, 21:08:03
+    Author     : emers
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <%@include file="WEB-INF/jspf/navbar.jspf" %>
+        <h1>Indice</h1>
+        <h2><a href="customers.jsp">Clientes</a></h2>
+        <h2><a href="#">Fabricantes</a></h2>
+    </body>
+</html>
---
+++ Aula12_JDBC/src/java/br/com/fatecpg/db/Manufacturer.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.db;
+
+/**
+ *
+ * @author emers
+ */
+public class Manufacturer {
+    private int id;
+    private String nome;
+    private String cidade;
+    private String estado;
+    private String email;
+
+    public Manufacturer(int id, String nome, String cidade, String estado, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
